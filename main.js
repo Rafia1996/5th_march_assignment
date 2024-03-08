ff78662e07ad6d03718a66f42b6bfd24f6fbf0dd
Rafia1996 <161371012+Rafia1996@users.noreply.github.com>
2024-03-08 17:37:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//camelcase
+var userName = 'owais';
+// snakecase
+var user_name = 'rafia';
+//parcalcase
+var UserName = 'rafia owais';
+console.log(userName, user_name, UserName);
+var isUserExit = true;
+var is_user_exit = false;
+var IsUserExit = 1;
+console.log(isUserExit, is_user_exit, IsUserExit);
+//Array
+var student = ["rafia", "rida", "yahya"];
+console.log(student[0], student[1], student[2]);
+//number array
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]);
+//boolean 
+var ten = 10;
+var twenty = 20;
+console.log("ten is equal to twenty");
+console.log(10 == twenty);
+console.log("ten is equal to twenty");
+console.log(10 != twenty);
---
+++ main.ts
@@ -0,0 +1,32 @@
+//camelcase
+let userName = 'owais'; 
+
+// snakecase
+let user_name = 'rafia';
+
+//parcalcase
+let UserName = 'rafia owais'; 
+
+console.log(userName, user_name, UserName);
+
+let isUserExit = true;
+let is_user_exit = false;
+let IsUserExit = 1;
+console.log(isUserExit, is_user_exit, IsUserExit);
+
+
+//Array
+let student: string[]=["rafia", "rida", "yahya"]
+console.log(student[0] ,student[1], student[2]);
+//number array
+let numbers : number[] = [1,2,3,4,5,6];
+console.log(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4] ,numbers[5]);
+//boolean 
+let ten=10;
+let twenty=20;
+
+console.log("ten is equal to twenty");
+console.log( 10 == twenty);
+
+console.log("ten is equal to twenty");
+console.log( 10 != twenty);
